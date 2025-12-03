70250ca08ab33661f3aeb1bfcf1e057aeff5a72f
Haran-01 <haransureshkumar.2006@gmail.com>
2025-12-03 12:36:17 +0530

    Add cleaned project folder

---
+++ Project-Messaging-Website/index.js
@@ -0,0 +1,1 @@
+console.log('Hi potta vijyakumar.I am Aaryan-sreeharikunju')
---
+++ Project-Messaging-Website/src/home.jsx
@@ -0,0 +1,13 @@
+import "./home.css"
+function Home(){
+    return(
+        <div className="home">
+            <div className="title">Messaging App</div>
+            <div className="btn-cont">
+                <button className="signin">Sign In</button>
+                <button className="login">Log In</button>
+            </div>
+        </div>
+    )
+}
+export default Home
